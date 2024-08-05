f83f7868638a5e669010c461fad1eb95af2e9ccd
Fred Mendes <fred.fmm@gmail.com>
2024-08-05 09:07:49 -0300

    Ajust gitIgnore and project name

--- target/classes/db/migration/common/V1.0__create_employee_ddl.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS employee
-(
-    id bigserial NOT NULL,
-    name VARCHAR(100) NOT NULL,
-    assignment VARCHAR(100) NOT NULL,
-    CONSTRAINT pk_employee PRIMARY KEY (id)
-);
